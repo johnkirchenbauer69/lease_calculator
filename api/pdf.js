b4f28077ea0927fec140d9c367bde75d0f044202
johnkirchenbauer69 <112353495+johnkirchenbauer69@users.noreply.github.com>
2025-11-11 13:04:33 -0600

    Update pdf.js

--- api/pdf.js
+++ api/pdf.js
@@ -1,11 +1,3 @@
-import streamProposalPdf from './ner-calculator/pdf/api-pdf.js';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  return streamProposalPdf(req, res);
-}
+import streamProposalPdf from '../ner-calculator/pdf/api-pdf.js';
+export const config = { api: { bodyParser: false } };
+export default function handler(req, res) { return streamProposalPdf(req, res); }
